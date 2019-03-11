3b0157ca9ea7707dbe74105f853665bc823150d2
Wayne Collier <ke4tri@gmail.com>
2019-03-11 10:40:18 -0500

    Add project files.

---
+++ ExpressionMemeber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExpressionMemeber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
